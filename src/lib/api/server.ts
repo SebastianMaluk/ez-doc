c794cf7b0208fc06a24eb56c6bba147d5567cfa6
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2023-07-06 04:11:07 -0400

    setup lib/api trpc server/client

---
+++ src/lib/api/server.ts
@@ -0,0 +1,20 @@
+// import { auth as getAuth } from "@clerk/nextjs/app-beta";
+import superjson from "superjson";
+import { createTRPCNextLayout } from "@/@trpc/next-layout/server";
+
+import "server-only";
+import { createContextInner } from "@/server/api/context";
+import { appRouter } from "@/server/api/root";
+
+export const api = createTRPCNextLayout({
+  router: appRouter,
+  transformer: superjson,
+  createContext() {
+    // TODO: add auth to context
+    // const auth = getAuth();
+    return createContextInner({
+      // auth,
+      req: null,
+    });
+  },
+});
