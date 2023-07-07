35f6020c42f8533b5a6f25a9273d1e06ace9836e
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2023-07-07 02:07:45 -0400

    prettier refactor

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,14 @@
-import type { Config } from "drizzle-kit";
-import "dotenv/config"
+import type { Config } from 'drizzle-kit'
+import 'dotenv/config'
 
 if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL environment variable not set");
+  throw new Error('DATABASE_URL environment variable not set')
 }
 
 export default {
-  schema: "./src/server/db/schema.ts",
-  driver: "mysql2",
+  schema: './src/server/db/schema.ts',
+  driver: 'mysql2',
   dbCredentials: {
     connectionString: process.env.DATABASE_URL
   }
-} satisfies Config;
+} satisfies Config
