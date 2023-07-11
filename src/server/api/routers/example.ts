fa7477085ab414a88cc2869e61905dd2a8480354
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2023-07-10 22:03:37 -0400

    remove unused trpc route

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,17 +1,10 @@
-import { desc } from 'drizzle-orm'
 import { z } from 'zod'
 import { createTRPCRouter, protectedProcedure, publicProcedure } from '@/server/api/trpc'
-import { notes } from '@/server/db/schema'
 
 export const exampleRouter = createTRPCRouter({
   hello: publicProcedure.input(z.object({ text: z.string() })).query(({ input }) => {
     return {
       greeting: `Hello ${input.text}`
     }
-  }),
-
-  getNotes: protectedProcedure.query(({ ctx: { db } }) => {
-    const data = db.select().from(notes).orderBy(desc(notes.created_at))
-    return data
   })
 })
