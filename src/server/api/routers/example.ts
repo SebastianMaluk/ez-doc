e46ff44156f5d48948bf45d3fd5a6e8e49a0e8d9
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2023-07-10 23:09:42 -0400

    delete unused vars

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod'
-import { createTRPCRouter, protectedProcedure, publicProcedure } from '@/server/api/trpc'
+import { createTRPCRouter, publicProcedure } from '@/server/api/trpc'
 
 export const exampleRouter = createTRPCRouter({
   hello: publicProcedure.input(z.object({ text: z.string() })).query(({ input }) => {
