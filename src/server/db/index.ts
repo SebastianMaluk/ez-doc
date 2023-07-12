c746872823871d4ba3895d5c099c241453545ee8
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2023-07-12 06:47:22 -0400

    add staging vars

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -3,7 +3,7 @@ import { drizzle } from 'drizzle-orm/planetscale-serverless'
 import { env } from '@/env.mjs'
 
 const connection = connect({
-  url: env.DATABASE_URL
+  url: env.DATABASE_URL_STAGING
 })
 
 export const db = drizzle(connection)
