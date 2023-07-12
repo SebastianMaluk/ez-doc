30b2eb8c7540e038f9558a89d5d3942445647988
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2023-07-12 06:49:57 -0400

    use table for showing exams

---
+++ src/app/exams/columns.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import { Button } from '@/components/ui/Button'
+import { ExamModel } from '@/server/db/schema'
+import { TagModel } from '@/server/db/schema'
+import { ColumnDef } from '@tanstack/react-table'
+import { ArrowUpDown } from 'lucide-react'
+
+// new model merging exam model id, name, and tag
+export interface ExamTagModel extends ExamModel {
+  tag: TagModel['name']
+}
+
+export const columns: ColumnDef<Pick<ExamTagModel, 'id' | 'name' | 'tag'>>[] = [
+  {
+    accessorKey: 'name',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          className='text-left'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Nombre
+          <ArrowUpDown className='ml-2 h-4 w-4' />
+        </Button>
+      )
+    },
+    cell: ({ row }) => {
+      const { name } = row.original
+      return <div className='text-left'> {name}</div>
+    }
+  },
+  {
+    accessorKey: 'tag',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          className='text-right'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Categoría
+          <ArrowUpDown className='ml-2 h-4 w-4' />
+        </Button>
+      )
+    },
+    cell: ({ row }) => {
+      const { tag } = row.original
+      return <div className='text-right'> {tag}</div>
+    }
+  }
+]
---
+++ src/app/exams/page.tsx
@@ -0,0 +1,31 @@
+import { DataTable } from '@/components/ui/Data-table'
+import { db } from '@/server/db'
+import { exams, tags, tagsToExams } from '@/server/db/schema'
+import { eq } from 'drizzle-orm'
+import { FC } from 'react'
+import { columns } from './columns'
+
+async function getData() {
+  const labsQuery = await db
+    .select({
+      id: exams.id,
+      name: exams.name,
+      tag: tags.name
+    })
+    .from(exams)
+    .innerJoin(tagsToExams, eq(tagsToExams.exam_id, exams.id))
+    .innerJoin(tags, eq(tagsToExams.tag_id, tags.id))
+    .where(eq(exams.type, 'laboratory'))
+  return labsQuery
+}
+
+const page: FC = async () => {
+  const data = await getData()
+  return (
+    <div className='mx-auto py-10 max-h-fit'>
+      <DataTable columns={columns} data={data} />
+    </div>
+  )
+}
+
+export default page
