b2644c9634bcf7a4c29fa577090d748c9401a75f
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2023-07-12 06:49:07 -0400

    remove border from navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = async () => {
   const session = await getServerSession(authOptions)
 
   return (
-    <div className='relative top-0 inset-x-0 bg-indigo-500 dark:bg-slate-800 border-b border-indigo-300 z-10 py-2'>
+    <div className='relative top-0 inset-x-0 bg-indigo-500 dark:bg-slate-800 z-10 py-2'>
       <div className='container max-w-7xl h-full mx-auto flex items-center justify-between gap-2'>
         <Link href={'/' as Route} className='flex flex-row gap-2 items-center'>
           <Stethoscope className='w-12 h-12 sm:h-8 sm:w-8 stroke-slate-100' />
