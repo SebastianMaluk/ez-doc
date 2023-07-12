1100db040b2651e38a692378db9ee0976145445d
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2023-07-12 06:46:03 -0400

    center sign in on modal

--- src/app/@authModal/(.)sign-in/page.tsx
+++ src/app/@authModal/(.)sign-in/page.tsx
@@ -6,12 +6,13 @@ const page: FC = () => {
   return (
     <div className='fixed inset-0 bg-white/20 dark:bg-slate-900/20 z-10'>
       <div className='container flex items-center h-full max-w-lg mx-auto'>
-        <div className='relative border-4 border-slate-700 w-full h-fit py-20 px-2 rounded-lg'>
+        <div className='relative border-4 border-slate-700 dark:bg-slate-900 bg-slate-100 w-full h-1/4 py-20 px-2 rounded-lg'>
           <div className='absolute top-4 right-4'>
             <CloseModal />
           </div>
-
-          <SignIn />
+          <div className='flex items-center justify-center h-full'>
+            <SignIn />
+          </div>
         </div>
       </div>
     </div>
