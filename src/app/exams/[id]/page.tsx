3f498551c46e1e1f8593992a771e1d4c37a5e41a
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2023-07-12 06:51:35 -0400

    show individual exam, needs work

---
+++ src/app/exams/[id]/page.tsx
@@ -0,0 +1,37 @@
+import Exam from '@/components/Exam'
+import { db } from '@/server/db'
+import { exams, tags, images, tagsToExams } from '@/server/db/schema'
+import { eq } from 'drizzle-orm'
+import { FC } from 'react'
+
+interface pageProps {
+  params: {
+    id: number
+  }
+}
+
+const page: FC<pageProps> = async ({ params }: pageProps) => {
+  const examQuery = await db
+    .select({
+      name: exams.name,
+      description: exams.description,
+      preparation: exams.preparation,
+      type: exams.type,
+      tag: tags.name,
+      laterality: images.laterality,
+      contrast: images.contrast
+    })
+    .from(exams)
+    .innerJoin(tagsToExams, eq(tagsToExams.exam_id, exams.id))
+    .innerJoin(tags, eq(tagsToExams.tag_id, tags.id))
+    .leftJoin(images, eq(images.exam_id, exams.id))
+    .where(eq(exams.id, params.id))
+  const examFound = examQuery[0]
+  return (
+    <div>
+      <Exam exam={examFound} />
+    </div>
+  )
+}
+
+export default page
---
+++ src/components/Exam.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import { FC } from 'react'
+
+interface examProps {
+  exam: {
+    name: string
+    description: string
+    preparation: string | null
+    type: 'image' | 'laboratory' | 'other'
+    tag: string
+    laterality: string | null
+    contrast: boolean | null
+  }
+}
+
+const Exam: FC<examProps> = ({ exam }) => {
+  if (!exam.preparation) exam.preparation = 'No requiere preparación'
+  if (!exam.laterality) exam.laterality = 'No aplica'
+  let tipo = ''
+  if (exam.type === 'image') tipo = 'Imagen'
+  if (exam.type === 'laboratory') tipo = 'Laboratorio'
+  return (
+    <div className='flex flex-col items-start justify-center space-y-4 p-6'>
+      <h1 className='text-4xl font-bold'>{exam.name}</h1>
+      <p className='text-xl'>
+        <strong>Descripción:</strong> {exam.description}
+      </p>
+      <p className='text-xl'>
+        <strong>Preparación:</strong> {exam.preparation}
+      </p>
+      <p className='text-xl'>
+        <strong>Tipo:</strong> {tipo}
+      </p>
+      <p className='text-xl'>
+        <strong>Categoría:</strong> {exam.tag}
+      </p>
+      <p className='text-xl'>
+        <strong>Lateralidad:</strong> {exam.laterality}
+      </p>
+      <p className='text-xl'>
+        <strong>Contraste:</strong> {exam.contrast ? 'Yes' : 'No'}
+      </p>
+    </div>
+  )
+}
+
+export default Exam
