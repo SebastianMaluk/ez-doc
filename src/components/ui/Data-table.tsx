984c47450447a42977fb1dd58d16ec42b0ae8605
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2023-07-12 06:49:32 -0400

    add table from shadcn/ui

---
+++ src/components/ui/Data-table.tsx
@@ -0,0 +1,124 @@
+'use client'
+
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable
+} from '@tanstack/react-table'
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from '@/components/ui/Table'
+
+import { DataTablePagination } from './Data-table-pagination'
+import { SyntheticEvent, useState } from 'react'
+import { Input } from '@/components/ui/Input'
+import { useRouter } from 'next/navigation'
+import { Route } from 'next'
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[]
+  data: TData[]
+}
+
+export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData, TValue>) {
+  const router = useRouter()
+  const [sorting, setSorting] = useState<SortingState>([])
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    state: {
+      sorting,
+      columnFilters
+    }
+  })
+
+  const clickRow = (event: SyntheticEvent) => {
+    // Define clickRow function inside the component.
+    event.preventDefault()
+    const rowId_str = event.currentTarget.getAttribute('data-rowid')
+    if (!rowId_str) return
+    const rowId = parseInt(rowId_str) + 1
+    router.push(`/exams/${rowId}` as Route) // Reference the router here.
+  }
+
+  return (
+    <div>
+      <div className='flex items-center py-4'>
+        <Input
+          placeholder='Filtro'
+          // For filtering by column
+          // value={(table.getColumn('name')?.getFilterValue() as string) ?? ''}
+          // onChange={(event) => table.getColumn('name')?.setFilterValue(event.target.value)}
+          // For filtering globally
+          onChange={(event) => table.setGlobalFilter(event.target.value)}
+          className='max-w-sm'
+        />
+      </div>
+      <div className='rounded-md border'>
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(header.column.columnDef.header, header.getContext())}
+                    </TableHead>
+                  )
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && 'selected'}
+                  data-rowid={row.id}
+                  onClick={clickRow}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={columns.length} className='h-24 text-center'>
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className='flex items-center justify-end space-x-2 py-4'>
+        <DataTablePagination table={table} />
+      </div>
+    </div>
+  )
+}
