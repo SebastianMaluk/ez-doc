e6656621705a7120c380f8bd2af945f66b18fa09
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2023-07-12 06:50:55 -0400

    set correct db url

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -3,7 +3,7 @@ import { drizzle } from 'drizzle-orm/planetscale-serverless'
 import { env } from '@/env.mjs'
 
 const connection = connect({
-  url: env.DATABASE_URL_STAGING
+  url: env.DATABASE_URL
 })
 
 export const db = drizzle(connection)
