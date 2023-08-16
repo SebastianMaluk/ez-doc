5f49cbe54acde6b5ddf96accce2177800412a3ac
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2023-08-15 20:50:24 -0400

    delete authModal feature

--- src/components/AuthOnNavbar.tsx
+++ src/components/AuthOnNavbar.tsx
@@ -1,25 +1,14 @@
-"use client"
-
-import { FC } from "react"
 import Link from "next/link"
-import { usePathname } from "next/navigation"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 
-const AuthOnNavbar: FC = () => {
-  const pathname = usePathname()
-  console.log(pathname)
+const AuthOnNavbar = () => {
+  
   return (
-    <>
-      {pathname?.includes("sign-in") ? (
-        <></>
-      ) : (
-        <Link href={`/sign-in`} className={cn(buttonVariants(), "dark:bg-slate-400")}>
-          <p className='dark:text-slate-800 text-xl font-medium cursor-pointer'>Ingresar</p>
-        </Link>
-      )}
-    </>
+    <Link href={`/sign-in`} className={cn(buttonVariants(), "dark:bg-slate-400")}>
+      <p className='dark:text-slate-800 text-xl font-medium cursor-pointer'>Ingresar</p>
+    </Link>
   )
 }
 
