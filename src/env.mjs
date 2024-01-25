8317ed175a8494e26a1742f058c91de9dcf913ba
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2024-01-25 01:32:32 -0300

    use t3 env

--- src/env.mjs
+++ src/env.mjs
@@ -1,76 +1,23 @@
-/* eslint-disable no-undef */
+import { createEnv } from "@t3-oss/env-nextjs"
 import { z } from "zod"
 
-//TODO: add clerk and next public keys
-const server = z.object({
-  NODE_ENV: z.enum(["development", "test", "production"]),
-  // CLERK_SECRET_KEY: z.string().min(1),
-  DATABASE_URL: z.string().min(1),
-  // DATABASE_URL_STAGING: z.string().min(1)
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z.string().url().default("postgres://postgres:postgres@localhost:5432/postgres"),
+    DATABASE_URL_STAGING: z
+      .string()
+      .url()
+      .default("postgres://postgres:postgres@localhost:5432/postgres"),
+    NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
+    NEXTAUTH_URL: z.string().url().default("http://localhost:3000"),
+    NEXTAUTH_SECRET: z.string(),
+    VERCEL_URL: z.string().default("localhost:3000"),
+    AUTH_GOOGLE_ID: z.string(),
+    AUTH_GOOGLE_SECRET: z.string(),
+  },
+  client: {
+    NEXTAUTH_URL: z.string(),
+  },
+  // For Next.js >= 13.4.4, you only need to destructure client variables:
+  experimental__runtimeEnv: {},
 })
-
-const client = z.object({
-  NEXT_PUBLIC_APP_URL: z.string().min(1),
-  // NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
-})
-
-/**
- * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
- * middlewares) or client-side so we need to destruct manually.
- *
- * @type {Record<keyof z.infer<typeof server> | keyof z.infer<typeof client>, string | undefined>}
- */
-
-const processEnv = {
-  NODE_ENV: process.env.NODE_ENV,
-  NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
-  // CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
-  // NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
-  //   process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-  DATABASE_URL: process.env.DATABASE_URL,
-  // DATABASE_URL_STAGING: process.env.DATABASE_URL_STAGING
-}
-
-// Don't touch the part below
-// --------------------------
-
-const merged = server.merge(client)
-
-/** @typedef {z.input<typeof merged>} MergedInput */
-/** @typedef {z.infer<typeof merged>} MergedOutput */
-/** @typedef {z.SafeParseReturnType<MergedInput, MergedOutput>} MergedSafeParseReturn */
-
-let env = /** @type {MergedOutput} */ (process.env)
-
-if (!!process.env.SKIP_ENV_VALIDATION == false) {
-  const isServer = typeof window === "undefined"
-
-  const parsed = /** @type {MergedSafeParseReturn} */ (
-    isServer
-      ? merged.safeParse(processEnv) // on server we can validate all env vars
-      : client.safeParse(processEnv) // on client we can only validate the ones that are exposed
-  )
-
-  if (parsed.success === false) {
-    console.log("processEnv", processEnv)
-    console.error("❌ Invalid environment variables:", parsed.error.flatten().fieldErrors)
-    throw new Error("Invalid environment variables")
-  }
-
-  env = new Proxy(parsed.data, {
-    get(target, prop) {
-      if (typeof prop !== "string") return undefined
-      // Throw a descriptive error if a server-side env var is accessed on the client
-      // Otherwise it would just be returning `undefined` and be annoying to debug
-      if (!isServer && !prop.startsWith("NEXT_PUBLIC_"))
-        throw new Error(
-          process.env.NODE_ENV === "production"
-            ? "❌ Attempted to access a server-side environment variable on the client"
-            : `❌ Attempted to access server-side environment variable '${prop}' on the client`
-        )
-      return target[/** @type {keyof typeof target} */ (prop)]
-    },
-  })
-}
-
-export { env }
