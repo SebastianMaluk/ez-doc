1f95790ecd7c9b60753918aaac632c84be72e93d
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2024-01-25 01:12:28 -0300

    delete trpc

--- src/lib/api/types.ts
+++
@@ -1,5 +0,0 @@
-import { type AppRouter } from "@/server/api/root"
-import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server"
-
-export type RouterOutputs = inferRouterOutputs<AppRouter>
-export type RouterInputs = inferRouterInputs<AppRouter>
